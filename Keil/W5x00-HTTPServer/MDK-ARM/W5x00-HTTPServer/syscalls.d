f3bb85766b45f1ec88a0d81e5ab3c93d018e498d
irina <irinakim1225@gmil.com>
2020-09-14 11:00:30 +0900

    NUCLEO-F103RB and W5500 HTTPServer Keil using CubeMX

---
+++ Keil/W5x00-HTTPServer/MDK-ARM/W5x00-HTTPServer/syscalls.d
@@ -0,0 +1,3 @@
+w5x00-httpserver\syscalls.o: ..\Src\syscalls.c
+w5x00-httpserver\syscalls.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+w5x00-httpserver\syscalls.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
